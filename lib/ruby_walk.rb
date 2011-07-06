e001b842680bdc7cfa10bf777bbf6f1a46d019cb
Maxime Bourget <mb14@sanger.ac.uk>
2011-07-06 21:49:30 +0800

    fix and refactor edges problems

--- lib/ruby_walk.rb
+++ lib/ruby_walk.rb
@@ -18,21 +18,34 @@ class Object
       end
     end
 
+    parent = parent_and_index && parent_and_index[0]
     # we should open the Array class, but for some reason , it doesn't work in Rails ???
     if is_a?(Array)
-      parent = parent_and_index && parent_and_index[0]
       max_index = size-1
       return each_with_index.map {|e, i| e.walk_objects(options, already_walked, [parent,i, max_index], &block)}.flatten
     end
 
-    # we compute a key to store if an object has already been loaded or not.can be the idea for transiant object
-    key = _compute_walk_key
-    #puts "walking #{key.inspect}"
-    return [] if already_walked[key] == true
 
-    #we skip object which the block has return nil
-    #but we propagate those which return []
-    
+    # here start the real code.
+    # the code look complicated, but the problem is too
+    key = _compute_walk_key
+    edge_key = _compute_walk_key(parent)
+
+    __object_walked, __edge_walked = [key, edge_key].map { |k| already_walked[k] }
+    object_walked = ( __object_walked == true)
+    object_cut = __object_walked == Cut
+    process_edge = block and block.arity > 1
+    edge_walked = __edge_walked or not process_edge
+
+
+
+    # we need to process it if the edge has been done
+    if object_walked and edge_walked
+      #everything needed has already been done
+      return []
+    end
+    already_walked[edge_key] = true if process_edge
+
     self_object = if block 
                     case block.arity
                     when 1 then block[self] 
@@ -44,18 +57,27 @@ class Object
                   else
                     self
                   end
-    # Hack to be able to process cut object only once, but do them properly if needed
-    # TODO do it better
-    if self_object.is_a?(Cut)
-      return [] if  already_walked[key]== Cut
-
-      already_walked[key] = Cut
-      return self_object.object
+    case self_object
+    when Cut
+      # no propagation
+      if object_walked or (object_cut and edge_walked)
+        []
+      else
+        #we return the real object, and mark it as already cut
+        already_walked[key] = Cut
+        [self_object.object]
+      end
+    when nil
+      return []
+    else
+      #standard case
+      already_walked[key] = true
+      if object_walked 
+        [self_object]
+      else
+      (_walk_objects(options, already_walked, parent_and_index, &block ). << self_object).flatten
+      end
     end
-    return [] if self_object.nil?
-    already_walked[key] = true
-
-    (_walk_objects(options, already_walked, parent_and_index, &block ). << self_object).flatten
   end
 
 
@@ -98,17 +120,17 @@ class Object
     end
     walked
   end
-  def _compute_walk_key()
-    object_id
+  def _compute_walk_key(parent=nil)
+    parent ? [object_id, parent._compute_walk_key]  : object_id
   end
 
 end
 
 if defined?(ActiveRecord)
   class ActiveRecord::Base
-    def _compute_walk_key()
+    def _compute_walk_key(parent=nil)
       # the idea of the object doesn't work, because an active record can be loaded many times into different object
-      key = [self.class.name, self.id]
+      key = [self.class.name, self.id, parent ? parent._compute_walk_key : nil].compact
     end
 
   end
